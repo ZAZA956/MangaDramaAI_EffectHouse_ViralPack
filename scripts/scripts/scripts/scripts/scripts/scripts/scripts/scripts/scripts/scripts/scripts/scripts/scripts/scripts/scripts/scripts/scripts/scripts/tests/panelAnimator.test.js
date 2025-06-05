be13c4de65319671b0201a16137b3433f646f736
ZAZA956 <errizroundoraze@gmail.com>
2025-06-05 02:18:51 +0100

    Create panelAnimator.test.js

---
+++ scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/tests/panelAnimator.test.js
@@ -0,0 +1,7 @@
+import { animatePanel } from "../scripts/panelAnimator.js";
+
+test("Should animate panel with given data", () => {
+  const panelData = { emotion: "happy", intensity: 0.8 };
+  animatePanel(panelData);
+  // Placeholder for assertions related to panel animation
+});
