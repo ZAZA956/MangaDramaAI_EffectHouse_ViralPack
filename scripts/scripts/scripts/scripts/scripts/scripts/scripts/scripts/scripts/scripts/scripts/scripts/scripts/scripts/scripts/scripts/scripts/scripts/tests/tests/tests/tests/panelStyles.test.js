0e2b48feb10d0c363c5fb8c6aca0daaf49958f92
ZAZA956 <errizroundoraze@gmail.com>
2025-06-05 02:20:45 +0100

    Create panelStyles.test.js

---
+++ scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/tests/tests/tests/tests/panelStyles.test.js
@@ -0,0 +1,7 @@
+import { selectPanelStyle } from "../scripts/panelStyles.js";
+
+test("Should apply specified panel style", () => {
+  const styleName = "shonen";
+  selectPanelStyle(styleName);
+  // Placeholder for assertions related to panel style application
+});
