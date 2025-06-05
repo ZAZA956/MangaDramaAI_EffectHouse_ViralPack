9d466796cbde981c256b404e4bdbc3eacbf1a7cd
ZAZA956 <errizroundoraze@gmail.com>
2025-06-05 02:22:22 +0100

    Create easterEggs.test.js

---
+++ scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/tests/tests/tests/tests/tests/tests/easterEggs.test.js
@@ -0,0 +1,7 @@
+import { triggerEasterEgg } from "../scripts/easterEggs.js";
+
+test("Should trigger Easter egg with keyword", () => {
+  const keyword = "surprise";
+  triggerEasterEgg(keyword);
+  // Placeholder for assertions related to Easter egg triggering
+});
