9dba0b33802fef31a037a6ff7f66e768ca6f555c
ZAZA956 <errizroundoraze@gmail.com>
2025-06-05 02:34:03 +0100

    Create customizableSpeechBubbles.test.js

---
+++ scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/scripts/tests/tests/tests/tests/tests/tests/tests/tests/tests/tests/tests/tests/tests/customizableSpeechBubbles.test.js
@@ -0,0 +1,7 @@
+import { customizeSpeechBubble } from "../scripts/customizableSpeechBubbles.js";
+
+test("Should customize speech bubble with given data", () => {
+  const bubbleData = { text: "Hello!", style: "comic" };
+  customizeSpeechBubble(bubbleData);
+  // Placeholder for assertions related to speech bubble customization
+});
